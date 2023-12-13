fca05d1ac5420cba39fb7f02f3c9bcd2f61e3adb
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-12-13 21:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
-    //Write your code here
-  
+	var rows = prompt("Input number of rows");
+	var cols = prompt("Input number of columns");
+	var table = document.getElementById('myTable');
+	var tbody = document.createElement("tbody");
+	for (var i = 0; i < rows; i++){
+		var row = document.createElement("tr");
+		for (var j = 0; j < cols; j++){
+			var cell = document.createElement("td");
+			var input = document.createElement("input");
+	        input.type = "text";
+            input.placeholder = "Row-" + i + " Column-" + j;
+	        cell.appendChild(input);
+			row.appendChild(cell);
+		}
+		tbody.appendChild(row);
+	}
+	table.appendChild(tbody);
 }
